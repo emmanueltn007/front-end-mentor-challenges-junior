a29125d0f8c292aaa05146d98067e1824d34ad45
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-02 21:31:35 +0200

    Made layout changes in the Footer

--- html-css-and-js/blogr-landing-page/src/components/Footer.jsx
+++ html-css-and-js/blogr-landing-page/src/components/Footer.jsx
@@ -1,15 +1,15 @@
 function Footer () {
     return (
-        <footer className="py-8 md:py-16 px-8 md:pl-16 bg-[hsl(240,10%,16%)] text-center flex flex-col md:flex-row items-center rounded-tr-4xl">
+        <footer className="py-8 md:py-16 px-8 md:pl-16 bg-[hsl(240,10%,16%)] text-center flex flex-col gap-16 md:flex-row max-md:items-center rounded-tr-[128px]">
             <div>
-                <img src="/images/logo.svg" alt="blogr logo image" />
+                <img className="h-8" src="/images/logo.svg" alt="blogr logo image" />
             </div>
-            <nav className="w-full text-white flex flex-col md:flex-row md:justify-evenly gap-4">
-                <div>
+            <nav className="w-full text-white flex flex-col md:flex-row md:justify-evenly gap-8">
+                <div className="flex flex-col gap-4">
                     <p className="font-bold">
                         Product
                     </p>
-                    <ul>
+                    <ul className="flex flex-col gap-1 text-gray-400">
                         <li>
                             Overview
                         </li>
@@ -27,11 +27,11 @@ function Footer () {
                         </li>
                     </ul>
                 </div>
-                <div>
+                <div className="flex flex-col gap-4">
                     <p className="font-bold">
                         Company
                     </p>
-                    <ul>
+                    <ul className="flex flex-col gap-1 text-gray-400">
                         <li>
                             About
                         </li>
@@ -46,11 +46,11 @@ function Footer () {
                         </li>
                     </ul>
                 </div>
-                <div>
+                <div className="flex flex-col gap-4">
                     <p className="font-bold">
                         Connect
                     </p>
-                    <ul>
+                    <ul className="flex flex-col gap-1 text-gray-400">
                         <li>
                             Contact
                         </li>
