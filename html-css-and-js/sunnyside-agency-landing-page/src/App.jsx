a61c9b47a2ea58a81696c8113686ef4b31fe6d39
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-06 14:16:01 +0200

    Added the Creatives.jsx

--- html-css-and-js/sunnyside-agency-landing-page/src/App.jsx
+++ html-css-and-js/sunnyside-agency-landing-page/src/App.jsx
@@ -1,16 +1,17 @@
 import CreativesSection from "./components/CreativesSection";
 import Header from "./components/Header";
 
-function App () {
-    return (
-        <>
-            <div className="bg-[url('/images/mobile/image-header.jpg')] bg-center bg-no-repeat bg-cover">
-                <Header />
-                <CreativesSection />
-            </div>
-            
-        </>
-    );
+function App() {
+  return (
+    <>
+      <div 
+        className="bg-[url('/images/mobile/image-header.jpg')] md:bg-[url('/images/desktop/image-header.jpg')] bg-center bg-no-repeat
+                    bg-cover">
+        <Header />
+        <CreativesSection />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- html-css-and-js/sunnyside-agency-landing-page/src/components/CreativesSection.jsx
+++ html-css-and-js/sunnyside-agency-landing-page/src/components/CreativesSection.jsx
@@ -1,7 +1,10 @@
 function CreativesSection () {
     return (
-        <section>
-            
+        <section className="text-center flex flex-col items-center text-white">
+            <h1 className="text-4xl">
+                WE ARE CREATIVES
+            </h1>
+            <img src="/images/icon-arrow-down.svg" alt="menu icon" />
         </section>
     );
 }
