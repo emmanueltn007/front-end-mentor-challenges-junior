7f281689d21d5bef38efe3353be2f7ad5e5b8433
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-10 18:58:41 +0200

    Made the components a grid in Features.jsx, used fillRule instead of fill-rule in Footer.jsx and Header.jsx, and lastly  fixed the issue of images not rendering

--- html-css-and-js/sunnyside-agency-landing-page/src/components/Features.jsx
+++ html-css-and-js/sunnyside-agency-landing-page/src/components/Features.jsx
@@ -1,6 +1,6 @@
 function Features ({ title, description, mobileImage, desktopImage, alt, reverse }) {
     return (
-        <section>
+        <section className="grid grid-rows-2 md:grid-cols-2 md:grid-rows-1">
             {/* IMAGE SECTION */}
             <div className={reverse ? "md:col-start-2" : ""}>
                 <img className="md:hidden" src={mobileImage} alt={alt} />
@@ -8,7 +8,7 @@ function Features ({ title, description, mobileImage, desktopImage, alt, reverse
             </div>
 
             {/* TEXT SECTION */}
-            <div className={`text-center flex flex-col justify-center gap-8 p-8 md:row-start-1 ${reverse ? 'md:col-start-1' : ''}`}>
+            <div className={`text-center flex flex-col justify-center gap-8 p-8 ${reverse ? 'md:row-start-1' : ''}`}>
                 <h2 className="text-2xl font-bold font-['Fraunces']">{title}</h2>
                 <p className="text-[hsl(232,10%,55%)] font-['Barlow']">{description}</p>
                 <button className="text-md font-bold font-['Fraunces']">LEARN MORE</button>
--- html-css-and-js/sunnyside-agency-landing-page/src/components/Photography&GraphicDesign.jsx
+++ html-css-and-js/sunnyside-agency-landing-page/src/components/Photography&GraphicDesign.jsx
@@ -5,8 +5,14 @@ function PhotographyGraphicDesign() {
     <section className="grid grid-rows-[500px_500px] md:grid-rows-[500px] md:grid-cols-2">
       {services.map(({ title, mobile_bg, desktop_bg, description }) => (
         <div
-          className={`bg-[url('${mobile_bg}')] md:bg-[url('${desktop_bg}')] 
-                            bg-center bg-no-repeat bg-cover flex flex-col text-center justify-end gap-4 p-16`}
+          key={title}
+          style={{
+            '--mobile-bg': `url(${mobile_bg})`,
+            '--desktop-bg': `url(${desktop_bg})`,
+            backgroundImage: 'var(--mobile-bg)',
+          }}
+          className="md:[background-image:var(--desktop-bg)]
+                            bg-center bg-no-repeat bg-cover flex flex-col text-center justify-end gap-4 p-16"
         >
           <h3 className="text-xl font-bold font-['Fraunces']">{title}</h3>
           <p className="text-[hsl(212,27%,19%)] font-['Barlow']">
