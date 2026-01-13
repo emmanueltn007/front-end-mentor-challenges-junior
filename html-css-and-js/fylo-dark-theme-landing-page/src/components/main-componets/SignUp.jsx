dd4a9dba2a55e8e72e0ba2e0bcec07277dfb78c8
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-13 14:13:15 +0200

    Added functionality in SignUp.jsx

--- html-css-and-js/fylo-dark-theme-landing-page/src/components/main-componets/SignUp.jsx
+++ html-css-and-js/fylo-dark-theme-landing-page/src/components/main-componets/SignUp.jsx
@@ -1,6 +1,30 @@
-
+import { useState } from "react";
 
 function SignUp() {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!validEmail.test(email)) {
+      setError("Please enter a valid email");
+      setSuccess("");
+    }
+    else {
+      setError("");
+      setSuccess("Email confirmation has been sent");
+
+      setTimeout(() => {
+        setSuccess("");
+      }, 2500);
+
+      setEmail("");
+    }
+  }
      
   return (
     <section className="rounded-md py-8 px-16 flex flex-col gap-4 text-white bg-[hsl(217,28%,15%)] shadow-xl shadow-gray-900 absolute left-8 right-8 md:left-56 md:right-56
@@ -11,13 +35,23 @@ function SignUp() {
         generous. If you have any questions, our support team would be happy to
         help you.
       </p>
-      <form className="flex flex-col md:flex-row gap-8" type="submit">
+      <form className="flex flex-col md:flex-row gap-8" onClick={handleSubmit}>
         <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           type="text"
           placeholder="email@example.com"
-          className="bg-[hsl(0,0%,100%)] text-gray-500 rounded-4xl md:rounded-5xl px-8 py-4 md:py-2 md:w-full"
+          className="outline-none bg-[hsl(0,0%,100%)] text-gray-500 rounded-4xl md:rounded-5xl px-8 py-4 md:py-2 md:w-full relative"
         />
+        {error && (
+          <p className="absolute bottom-4 left-24 text-[hsl(0,100%,63%)] text-xs font-semibold">{error}</p>
+        )}
+        {success && (
+          <p className="absolute bottom-4 left-24 text-[hsl(107,100%,63%)] text-xs font-semibold">{success}</p>
+        )}
+        
         <button 
+            type="submit"
             className="px-8 py-4 md:py-2 bg-[linear-gradient(90deg,hsl(176,68%,64%),hsl(198,60%,50%))] rounded-4xl font-bold cursor-pointer"
         >
           Get Started For Free
