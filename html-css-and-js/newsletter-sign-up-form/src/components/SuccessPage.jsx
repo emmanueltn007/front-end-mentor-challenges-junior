e9338b1dbbfada494f71a5b064fc7785e69cdc66
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-15 15:33:16 +0200

    Added some styling to SuccessPage.jsx

--- html-css-and-js/newsletter-sign-up-form/src/components/SuccessPage.jsx
+++ html-css-and-js/newsletter-sign-up-form/src/components/SuccessPage.jsx
@@ -1,13 +1,14 @@
 function SuccessPage () {
   return (
-    <section className="bg-[hsl(0,0%,100%)]">
-      <img src="/assets/images/icon-success.svg" alt="success icon" />
-      <h1>Thanks for subscribing!</h1>
+    <section className="bg-[hsl(0,0%,100%)] md:rounded-2xl flex flex-col gap-4 py-8 px-4 md:w-1/2">
+      <img className="self-start" src="/assets/images/icon-success.svg" alt="success icon" />
+      <h1 className="text-3xl font-bold">Thanks for subscribing!</h1>
       <p>
         A confirmation email has been sent to .... . Please open it and click the button inside it to
         confirm your subscription.
       </p>
-      <button>
+      <button className="bg-[hsl(235,18%,26%)] text-[hsl(0,0%,100%)] font-semibold py-2 rounded-md
+                          cursor-pointer max-md:mt-16">
         Dismiss message
       </button>
     </section>
