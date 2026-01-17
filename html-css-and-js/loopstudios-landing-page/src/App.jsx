2e51b0f7fe39255911b2350f012564b7cd423296
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-17 13:59:00 +0200

    Added Hero.jsx

--- html-css-and-js/loopstudios-landing-page/src/App.jsx
+++ html-css-and-js/loopstudios-landing-page/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 function App () {
   return (
     <div className="min-h-screen w-screen">
-      <div className="bg-[url('/images/mobile/image-hero.jpg')] md:bg-[url('/images/desktop/image-hero.jpg')]">
+      <div className="bg-[url('/images/mobile/image-hero.jpg')] md:bg-[url('/images/desktop/image-hero.jpg')] bg-center bg-no-repeat bg-size-[100%,100%]">
         <Header />
+        <Hero />
       </div>
       
     </div>
