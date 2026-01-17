0fd16d8b42eb5b5f50acfa1168af1fb0b6eb4237
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-17 17:42:04 +0200

    Added hover states in MobileNav.jsx

--- html-css-and-js/loopstudios-landing-page/src/components/header-and-footer-components/MobileNav.jsx
+++ html-css-and-js/loopstudios-landing-page/src/components/header-and-footer-components/MobileNav.jsx
@@ -15,7 +15,7 @@ function MobileNav ({isOpen, setIsOpen}) {
           setIsOpen={setIsOpen}
           containerClass={`absolute top-0 left-0 h-screen w-screen transition-transform duration-300 ease-in-out bg-[hsl(0,0%,0%)] z-100 ${isOpen ? "translate-x-0" : "translate-x-full"}`}
           listClass="flex flex-col gap-4 px-8 uppercase text-2xl"
-          itemsClass="text-[hsl(0,0%,85%)]" 
+          itemsClass="text-[hsl(0,0%,85%)] hover:text-[hsl(0,0%,100%)] relative w-fit after:content-[''] after:absolute after:-bottom-1 after:left-0 after:w-0 after:h-0.5 after:bg-[hsl(0,0%,100%)] hover:after:w-full after:transition-all after:duration-300 after:ease-in-out" 
         />
       </nav>
   );
