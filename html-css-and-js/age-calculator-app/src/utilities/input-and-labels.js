f2a62d82c5d80748c2a9edd421b304b8570c07b6
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-19 12:02:49 +0200

    Added interactivity with JavaScript on age-calculator-app.
    Added new project, insure-landing-app

---
+++ html-css-and-js/age-calculator-app/src/utilities/input-and-labels.js
@@ -0,0 +1,7 @@
+const inputsAndLabels = [
+    {label: "DAY", placeholder: "DD", id: "day-input", value: "dayInput"},
+    {label: "MONTH", placeholder: "MM", id: "month-input", value: "monthInput"},
+    {label: "YEAR", placeholder: "YYYY", id: "year-input", value: "yearInput"}
+];
+
+export default inputsAndLabels
