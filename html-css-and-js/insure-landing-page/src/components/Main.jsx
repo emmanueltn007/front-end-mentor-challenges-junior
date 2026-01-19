6916092a27b1a2cc42c5cb5c0c34a28f288e68b1
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-19 15:43:52 +0200

    Added MoreInfo.jsx as sub-component of Main.jsx

--- html-css-and-js/insure-landing-page/src/components/Main.jsx
+++ html-css-and-js/insure-landing-page/src/components/Main.jsx
@@ -1,11 +1,13 @@
 import Benefits from "./main-sub-components/Benefits";
 import Intro from "./main-sub-components/Intro";
+import MoreInfo from "./main-sub-components/MoreInfo";
 
 function Main () {
     return (
         <main>
             <Intro />
             <Benefits />
+            <MoreInfo />
         </main>
     );
 }
---
+++ html-css-and-js/insure-landing-page/src/components/main-sub-components/MoreInfo.jsx
@@ -0,0 +1,10 @@
+function MoreInfo () {
+    return (
+        <section className="bg-[hsl(256,26%,20%)] text-[hsl(0,0%,98%)] mx-8 md:mx-16 lg:mx-32 flex flex-col md:grid md:grid-cols-2 gap-8 px-8 md:px-16 py-16 my-32">
+            <h1 className="text-5xl font-bold max-md:text-center">Find Out More About How We Work</h1>
+            <button className="border-2 border-[hsl(0,0%,98%)] hover:text-[hsl(256,26%,20%)] hover:bg-[hsl(0,0%,98%)] self-start py-2 px-6 font-semibold cursor-pointer transition-all duration-300 ease-in-out md:w-1/2 md:ml-auto md:my-auto max-md:mx-auto">HOW WE WORK</button>
+        </section>
+    );
+}
+
+export default MoreInfo
