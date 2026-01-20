69652403bf5cd118dd61adcb5c4913a80b6d4577
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-20 13:20:27 +0200

    Added more background images

--- html-css-and-js/insure-landing-page/src/components/Main.jsx
+++ html-css-and-js/insure-landing-page/src/components/Main.jsx
@@ -4,7 +4,7 @@ import MoreInfo from "./main-sub-components/MoreInfo";
 
 function Main () {
     return (
-        <main>
+        <main className="max-md:bg-[url('/images/bg-pattern-intro-right-mobile.svg')] md:bg-[url('/images/bg-pattern-intro-left-desktop.svg')] bg-size-[150px] bg-no-repeat bg-right md:bg-left">
             <Intro />
             <Benefits />
             <MoreInfo />
--- html-css-and-js/insure-landing-page/src/components/main-sub-components/Intro.jsx
+++ html-css-and-js/insure-landing-page/src/components/main-sub-components/Intro.jsx
@@ -1,11 +1,11 @@
 function Intro () {
     return (
-        <section className="grid max-md:grid-rows-2 md:grid-cols-2 bg-[hsl(256,26%,20%)] md:p-16 lg:p-32 relative z-10">
+        <section className="grid max-md:grid-rows-2 md:grid-cols-2 bg-[hsl(256,26%,20%)] md:bg-[url('/images/bg-pattern-intro-right-desktop.svg')] bg-size-[400px] bg-no-repeat bg-top-right md:p-16 lg:p-32 relative z-10">
             <div className="h-full w-full md:col-start-2">
                 <img className="md:hidden object-cover h-full w-full" src="/images/image-intro-mobile.jpg" alt="intro image" />
-                <img className="max-md:hidden object-cover lg:absolute lg:right-32 lg:-translate-y-1/4 lg:top-2/5 lg:w-2/5" src="/images/image-intro-desktop.jpg" alt="intro image" />
+                <img className="-z-100 max-md:hidden object-cover lg:absolute lg:right-32 lg:-translate-y-1/4 lg:top-2/5 lg:w-2/5" src="/images/image-intro-desktop.jpg" alt="intro image" />
             </div>
-            <div className="text-[hsl(0,0%,98%)] max-md:text-center flex flex-col gap-6 justify-center max-md:px-8 md:row-start-1">
+            <div className="text-[hsl(0,0%,98%)] max-md:text-center flex flex-col gap-6 justify-center max-md:px-8 md:row-start-1 max-md:bg-[url('/images/bg-pattern-intro-left-mobile.svg')] bg-size-[250px] bg-no-repeat bg-top-left">
                 <h1 className="text-5xl font-bold">
                     Humanizing your insurance.
                 </h1>
