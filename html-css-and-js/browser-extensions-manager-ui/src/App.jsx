ad0f1f783132feef83bf2468e6941bca2030628c
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-21 16:26:06 +0200

    Added the Header.jsx with dark and light mode toggle

--- html-css-and-js/browser-extensions-manager-ui/src/App.jsx
+++ html-css-and-js/browser-extensions-manager-ui/src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App () {
+
+    const [isDark, setIsDark] = useState(false);
+
+    function handleDarkModeToggle () {
+        setIsDark(prev => !prev)
+    }
+
     return (
-        <div>
-            <Header />
+        <div className="p-4">
+            <Header isDark={isDark} handleDarkModeToggle={handleDarkModeToggle} />
             <Main />
         </div>
     );
