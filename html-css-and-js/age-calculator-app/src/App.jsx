41ba9bbedd5c0375ade9601a9aa98f8315c0ee16
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2026-01-26 19:09:52 +0200

    centered the whole container. removed the spinner arrow buttons

--- html-css-and-js/age-calculator-app/src/App.jsx
+++ html-css-and-js/age-calculator-app/src/App.jsx
@@ -52,7 +52,7 @@ function App() {
   }
 
   return (
-    <div className="h-screen bg-[hsl(0,0%,86%)]">
+    <div className="h-screen bg-[hsl(0,0%,86%)] flex items-center justify-center">
       <div className="flex flex-col gap-8 bg-[hsl(0,0%,100%)] px-8 py-16 md:py-8 w-4/5 md:w-3/5 lg:w-2/5 mx-auto rounded-br-[128px] rounded-bl-4xl rounded-t-4xl">
         
         <Form date={date} setDate={setDate} errors={errors} calculateAge={calculateAge} />
